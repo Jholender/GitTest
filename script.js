2ae35b44d62c59d1e1fed8169914ead6b0cfa116
Jarek Holender <jholender@op.pl>
2017-08-19 14:52:15 +0200

    zmianka

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ Cylinder.prototype.Valume = function() {
 	var radius = this.cyl_diameter /2
 	return this.cyl_heigh * cyl_diameter * Math.PI * radius * radius;
 };
-	var cyl = new cylinder(7,4);
+	var cyl = new cylinder(8,4);
 	console.log ('volume=', cyl.Valume().toFixed(4));
