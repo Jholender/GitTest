13ec00bd28a03638585a8d8e1266aae346d24de3
Jarek Holender <jholender@op.pl>
2017-08-19 14:45:02 +0200

    start

---
+++ script.js
@@ -0,0 +1,10 @@
+function Cylinder(cyl_heigh, cyl_diameter){
+	this.cyl_heigh = cyl_heigh;
+	this.cyl_diameter = cyl_diameter; 	
+}
+Cylinder.prototype.Valume = function() {
+	var radius = this.cyl_diameter /2
+	return this.cyl_heigh * cyl_diameter * Math.PI * radius * radius;
+};
+	var cyl = new cylinder(7,4);
+	console.log ('volume=', cyl.Valume().toFixed(4));
